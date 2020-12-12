2764a1b06acec62dd67b6d19d64780825d325a3a
Thestickman391 <stickman391@gmail.com>
2020-12-12 14:58:25 +1300

    Temp fix for autoplay on Chrome

--- scripts.js
+++ scripts.js
@@ -69,3 +69,66 @@ function changeMe()
 	}, 10);
 }
 changeMe();
+
+var isChromium = window.chrome;
+var winNav = window.navigator;
+var vendorName = winNav.vendor;
+var isOpera = typeof window.opr !== "undefined";
+var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+var isIOSChrome = winNav.userAgent.match("CriOS");
+
+if(
+  isChromium !== null &&
+  typeof isChromium !== "undefined"
+) {
+   document.getElementById("start").style.display = "flex";
+} else {
+   document.getElementById("start").style.display = "none";
+}
+
+var button = document.getElementById("start");
+button.onclick = function() {
+  	function playSoundScream(sound)
+	{
+		let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', sound);
+		xhr.responseType = 'arraybuffer';
+		xhr.addEventListener('load', () => {
+			let playsound = (audioBuffer) => {
+				let source = audioCtx.createBufferSource();
+				source.buffer = audioBuffer;
+				source.connect(audioCtx.destination);
+				source.loop = false;
+				source.start();
+				source.addEventListener("ended", function () {playSoundScream('sounds/screams/scream_' + ("0" + Math.floor(Math.random() * 88)).slice(-2) + '.mp3');}, false);
+			};
+
+			audioCtx.decodeAudioData(xhr.response).then(playsound);
+		});
+		xhr.send();
+	}	
+	function playSoundFart(sound)
+	{
+		let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', sound);
+		xhr.responseType = 'arraybuffer';
+		xhr.addEventListener('load', () => {
+			let playsound = (audioBuffer) => {
+				let source = audioCtx.createBufferSource();
+				source.buffer = audioBuffer;
+				source.connect(audioCtx.destination);
+				source.loop = false;
+				source.start();
+				source.addEventListener("ended", function () {playSoundFart('sounds/farts/fart_' + ("0" + Math.floor(Math.random() * 75)).slice(-2) + '.mp3');}, false);
+			};
+
+			audioCtx.decodeAudioData(xhr.response).then(playsound);
+		});
+		xhr.send();
+	}
+	
+	playSoundScream('sounds/screams/scream_' + ("0" + Math.floor(Math.random() * 88)).slice(-2) + '.mp3');
+	playSoundFart('sounds/farts/fart_' + ("0" + Math.floor(Math.random() * 75)).slice(-2) + '.mp3');
+}
